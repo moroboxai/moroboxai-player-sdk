431d2bba7c9baed2914f5393ff89350f76a0c5ad
Nauja <jeremymorosi@hotmail.fr>
2022-04-14 18:59:51 +0200

    fix test

--- test/index.ts
+++ test/index.ts
@@ -2,10 +2,12 @@ import { expect } from 'chai';
 import 'chai';
 import * as MoroboxAIPlayerSDK from '../src/';
 
+const packageJson = require('../package.json');
+
 describe('MoroboxAIPlayerSDK', function ()
 {
     it('should have VERSION', function ()
     {
-        expect(MoroboxAIPlayerSDK.VERSION).to.be.equal('0.1.0-alpha.2');
+        expect(MoroboxAIPlayerSDK.VERSION).to.be.equal(packageJson.version);
     });
 });
