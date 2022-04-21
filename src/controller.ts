32c08b94d442b5ed31dfdfcd5727294e5c6658d1
Nauja <jeremymorosi@hotmail.fr>
2022-04-21 19:28:29 +0200

    ai controller

--- src/controller.ts
+++ src/controller.ts
@@ -43,6 +43,59 @@ export interface IController extends MoroboxAIGameSDK.IController {
     unloadAI(): void;
 }
 
+class AIController implements IInputController {
+    private _context: {
+        update?: (state: any) => void
+    } = {};
+    private _input: Inputs = {};
+
+    get isBound(): boolean {
+        return this._context.update !== undefined;
+    }
+
+    get label(): string {
+        return 'AI';
+    }
+
+    loadAI(type: string, code?: string) {
+        this.remove();
+
+        if (code === undefined) {
+            code = type;
+            type = 'javascript';
+        }
+        
+        try {
+            (new Function('exports', 'sendInputs', code))(this._context, (state: any) => this._sendInput(state));
+        } catch(e) {
+            this.remove();
+        }
+    }
+
+    remove(): void {
+        this._context = {};
+        this._input = {};
+    }
+
+    sendState(state: any): void {
+        if (this._context.update !== undefined) {
+            try {
+                this._context.update(state);
+            } catch(e) {
+
+            }
+        }
+    }
+
+    inputs(): Inputs {
+        return this._input;
+    }
+
+    private _sendInput(state: any): void {
+        this._input = state;
+    }
+}
+
 // Allow to smoothly load and unload AI code to override inputs
 class Controller implements IController {
     // Unique identifier
@@ -51,10 +104,8 @@ class Controller implements IController {
     // Controller for receiving player inputs if human controller
     private _inputController?: IInputController;
 
-    private _ai: {
-        update?: (state: any) => void
-    } = {};
-    private _input = {};
+    // Controller for user defined AI
+    private _aiController: AIController;
 
     get id(): number {
         return this._id;
@@ -65,6 +116,10 @@ class Controller implements IController {
     }
 
     get label(): string {
+        if (this._aiController.isBound) {
+            return this._aiController.label;
+        }
+
         if (this._inputController !== undefined) {
             return 'human';
         }
@@ -75,38 +130,31 @@ class Controller implements IController {
     constructor(id: number, inputController?: IInputController) {
         this._id = id;
         this._inputController = inputController;
+        this._aiController = new AIController();
     }
 
     sendState(state: any): void {
-        if (this._ai.update !== undefined) {
-            this._ai.update(state);
-        }
+        this._aiController.sendState(state);
     }
 
     inputs(): any {
+        if (this._aiController.isBound) {
+            return this._aiController.inputs();
+        }
+
         if (this._inputController !== undefined) {
             return this._inputController.inputs();
         }
         
-        return this._input;
+        return {};
     }
 
     loadAI(type: string, code?: string): void {
-        if (code === undefined) {
-            code = type;
-            type = 'javascript';
-        }
-        
-        (new Function('exports', 'sendInput', code))(this._ai, (state: any) => this._sendInput(state));
+        this._aiController.loadAI(type, code);
     }
 
     unloadAI(): void {
-        this._ai = {};
-        this._input = {};
-    }
-
-    private _sendInput(state: any): void {
-        this._input = state;
+        this._aiController.remove();
     }
 }
 
