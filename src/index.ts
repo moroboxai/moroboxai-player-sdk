680ed23d737e4cfceed580ee69a6f47796444ac3
Nauja <jeremymorosi@hotmail.fr>
2023-09-16 08:36:29 +0200

    improve build

--- src/index.ts
+++ src/index.ts
@@ -24,7 +24,7 @@ export { VERSION as GAME_SDK_VERSION } from "moroboxai-game-sdk";
 /**
  * Version of the SDK.
  */
-export const VERSION: string = "0.1.0-alpha.24";
+export const VERSION: string = "0.1.0-alpha.25";
 
 // Force displaying the loading screen for x seconds
 const FORCE_LOADING_TIME = 1000;
@@ -505,6 +505,7 @@ class Player implements IPlayer, MoroboxAIGameSDK.IPlayer {
                 }
 
                 console.log("boot loaded");
+                const p = this._exports.boot(this._proxy);
                 this._exports.boot(this._proxy).then((game) => {
                     this._game = game;
                     this._game.ticker = this._tickFromGame;
--- vite.config.js
+++ vite.config.js
@@ -1,27 +1,5 @@
 import { defineConfig } from "vite";
-const { resolve } = require("path");
 
 export default defineConfig({
-    build: {
-        outDir: resolve(__dirname, "lib"),
-        lib: {
-            entry: resolve(__dirname, "src", "index.ts"),
-            name: "MoroboxAIPlayerSDK",
-            formats: ["cjs", "umd"],
-            fileName: (format) => {
-                switch (format) {
-                    case "cjs":
-                        return `cjs/index.cjs`;
-                    case "umd":
-                        return `umd/moroboxai-player-sdk.min.js`;
-                }
-            }
-        },
-        rollupOptions: {
-            input: {
-                main: "./index.html"
-            }
-        }
-    },
     base: "/moroboxai-player-sdk/"
 });
